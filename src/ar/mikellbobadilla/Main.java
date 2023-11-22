6025e010c61932915cd83d74a6021f3f009001e2
sting <bobadillasting@gmail.com>
2023-11-21 21:05:09 -0300

    [new] class Empleado

--- src/ar/mikellbobadilla/Main.java
+++ src/ar/mikellbobadilla/Main.java
@@ -1,26 +1,18 @@
 package ar.mikellbobadilla;
 
 
-import ar.mikellbobadilla.cola.Queue;
-import ar.mikellbobadilla.pila.Stack;
-
-import java.util.Iterator;
+import ar.mikellbobadilla.clases.Empleado;
 
 public class Main {
 
     public static void main(String[] args) {
+        Empleado e1 = new Empleado(20, "Pepito Pérez", "Ventas", "A-252");
+        Empleado e2 = new Empleado(44, "Ana López", "Recursos Humanos", "B-198");
+        Empleado e3 = new Empleado(18, "Elena Nito", "Ventas", "A-241");
+        Empleado e4 = new Empleado(33, "José Gómez", "Contabilidad", "B-96");
+        Empleado e5 = new Empleado(64, "Francisco Pérez", "Marketing", "A-69");
+        Empleado e6 = new Empleado(55, "Javier López", "Informática", "B-161");
 
-        Stack<String> names = new Stack<>();
 
-        names.push("Primer comentario");
-        names.push("Segundo comentario");
-        names.push("Tercer comentario");
-        names.push("Cuarto comentario");
-        names.push("Quinto comentario");
-
-        while (!names.isEmpty())
-            System.out.println(names.pop());
-
-        System.out.println(names.size());
     }
 }
---
+++ src/ar/mikellbobadilla/clases/Empleado.java
@@ -0,0 +1,72 @@
+package ar.mikellbobadilla.clases;
+
+import java.util.Objects;
+
+public class Empleado implements Comparable<Empleado> {
+    private int id;
+    private String nombre;
+    private String puesto;
+    private String despacho;
+
+    public Empleado() {
+    }
+
+    public Empleado(int id, String nombre, String puesto, String despacho) {
+        this.id = id;
+        this.nombre = nombre;
+        this.puesto = puesto;
+        this.despacho = despacho;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    public String getDespacho() {
+        return despacho;
+    }
+
+    public void setDespacho(String despacho) {
+        this.despacho = despacho;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Empleado empleado = (Empleado) o;
+        return id == empleado.id && Objects.equals(nombre, empleado.nombre) && Objects.equals(puesto, empleado.puesto) && Objects.equals(despacho, empleado.despacho);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, nombre, puesto, despacho);
+    }
+
+    @Override
+    public int compareTo(Empleado o) {
+        if (id == o.getId()) return 0;
+        else if (id < o.getId()) return -1;
+        else return 1;
+    }
+}
